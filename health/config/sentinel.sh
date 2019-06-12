ff3872c43b37179857606085ce99ce1eda0ca789
Prashant Patel <12061@sbigeneral.in>
2019-06-12 16:13:28 +0530

    removed k8s file as helm is used

--- health/config/sentinel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp /redis-config/sentinel.conf /sentinel.conf
-while ! ping -c 1 redis-premium-master-0.redissvc.default.svc.cluster.local; do
-    echo 'Waiting for server'
-    sleep 1
-done
-
-redis-sentinel /sentinel.conf
